654f81e874bfc59d808dccd7fc44ac553c59c7b0
DemarioDouce <demario.douce567@gmail.com>
2021-03-12 19:11:58 -0500

    Added activities.

--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab4_ex1/MainActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab4_ex1/MainActivity.java
@@ -2,7 +2,9 @@ package com.demariodouce.demariodouce_comp304sec003_lab4_ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Opens the NurseLoginActivity.
+    public void openNurseLogin(View view) {
+        Intent intent = new Intent(this, NurseLoginActivity.class);
+        startActivity(intent);
+    }
+
+    //Opens the Nurse Register Activity.
+    public void openNurseRegister(View view) {
+        Intent intent = new Intent(this, NurseRegisterActivity.class);
+        startActivity(intent);
     }
 }
