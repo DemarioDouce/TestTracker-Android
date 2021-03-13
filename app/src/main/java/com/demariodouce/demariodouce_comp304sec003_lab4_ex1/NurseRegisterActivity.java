5bcc5ad76fe3f70f9b5fb97f8b00e1d332426a61
DemarioDouce <demario.douce567@gmail.com>
2021-03-12 22:40:12 -0500

    Created register layout.

--- app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab4_ex1/NurseRegisterActivity.java
+++ app/src/main/java/com/demariodouce/demariodouce_comp304sec003_lab4_ex1/NurseRegisterActivity.java
@@ -3,6 +3,7 @@ package com.demariodouce.demariodouce_comp304sec003_lab4_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class NurseRegisterActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class NurseRegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nurse_register);
+    }
+
+    public void nurseRegister(View view) {
     }
 }
